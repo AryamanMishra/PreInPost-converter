b257ff7236d2b7ecb3641871d96b7b6d903835ff
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-05-31 13:54:29 +0530

    obtained values from form data

--- src/main/java/com/preinpost/controllers/OutputController.java
+++ src/main/java/com/preinpost/controllers/OutputController.java
@@ -1,9 +1,12 @@
 package com.preinpost.controllers;
 
+import java.util.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class OutputController {
@@ -12,4 +15,60 @@ public class OutputController {
     public String home() {
         return "output";
     }
+
+    @PostMapping("/output")
+    public String check(@RequestBody String form,
+                        Model model) {
+        boolean check;
+        String exp = getexp(form);
+        String exp_type = getexpType(form);
+        if (exp_type.equals("infix")) {
+            check = infix_check(exp);
+        }
+        else if (exp_type.equals("postfix")) {
+            check = postfix_check(exp);
+        }
+        else {
+            check = prefix_check(exp);
+        }
+        model.addAttribute("check",check);
+        return "output";
+    }
+
+    public String getexp(String form) {
+        int idx_ampersand = 0;
+        int idx_equal_to = 0;
+        for (int i=0;i<form.length();i++) {
+            if (form.charAt(i) == '=') {
+                idx_equal_to = i;
+                break;
+            }
+        }
+        for (int i=0;i<form.length();i++) {
+            if (form.charAt(i) == '&')
+                idx_ampersand = i;
+        }
+        String exp = form.substring(idx_equal_to+1,idx_ampersand);
+        return exp;
+    }
+    public String getexpType(String form) {
+        int idx_equal_to = 0;
+        for (int i=0;i<form.length();i++) {
+            if (form.charAt(i) == '=')
+                idx_equal_to = i;
+        }
+        String exp_type = form.substring(idx_equal_to+1);
+        return exp_type;
+    }
+
+    public boolean infix_check(String exp) {
+        return true;
+    }
+    public boolean postfix_check(String exp) {
+        return false;
+    }
+    public boolean prefix_check(String exp) {
+        return false;
+    }
+
 }
